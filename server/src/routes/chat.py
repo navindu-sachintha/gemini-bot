df2c67e52dd434e3e66af76a2741fe99a0658988
navindu-sachintha <navindu.social@gmail.com>
2023-12-23 09:52:07 +0530

    Feature: Create token generator

--- server/src/routes/chat.py
+++ server/src/routes/chat.py
@@ -1,5 +1,6 @@
 import os 
-from fastapi import APIRouter, FastAPI, WebSocket, Request
+from fastapi import APIRouter, FastAPI, WebSocket, Request, BackgroundTasks, HTTPException
+import uuid
 
 chat = APIRouter()
 
@@ -8,8 +9,16 @@ chat = APIRouter()
 # @access  Public
 
 @chat.post("/token")
-async def token_generator(request: Request):
-    return None
+async def token_generator(name:str, request: Request):
+    if name == "":
+        raise HTTPException(status_code=400, detail={
+            "loc": "name",  "msg": "Enter a valid name"})
+
+    token = str(uuid.uuid4())
+
+    data = {"name": name, "token": token}
+
+    return data
 
 
 # @route   POST /refresh_token
